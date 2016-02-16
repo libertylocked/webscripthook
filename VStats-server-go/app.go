2f3027562a0b5689cfc7e379094e0cc659cf7e6b
libertylocked <unlock.liberty@gmail.com>
2016-02-16 17:22:10 -0500

    Specify HTTP methods

--- VStats-server-go/app.go
+++ VStats-server-go/app.go
@@ -9,26 +9,29 @@ import (
 )
 
 var port = ""
-var router = mux.NewRouter()
 
 func main() {
 	port = getPort()
 	printCredits()
 
-	router.HandleFunc("/pull", handleGetJSON)
-	router.HandleFunc("/connected", handleGetPluginConnected)
-	// for plugin to upload/download data using websocket
-	router.HandleFunc("/pushws", handlePluginWS)
-	// for browser to send input to plugin
-	router.HandleFunc("/input", handlePostInput)
-	router.HandleFunc("/dummy", handleGetDummyJSON)
-	router.HandleFunc("/", handleIndex)
-
-	http.Handle("/", router)
+	http.Handle("/", getHandlers())
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.Handle("/skins/", http.StripPrefix("/skins/", http.FileServer(http.Dir("skins"))))
 	log.Println("Listening on port " + port)
 	http.ListenAndServe(":"+port, nil)
+}
+
+func getHandlers() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/pull", handleGetJSON).Methods("GET")
+	router.HandleFunc("/connected", handleGetPluginConnected).Methods("GET")
+	// for plugin to upload/download data using websocket
+	router.HandleFunc("/pushws", handlePluginWS)
+	// for browser to send input to plugin
+	router.HandleFunc("/input", handlePostInput).Methods("POST")
+	router.HandleFunc("/dummy", handleGetDummyJSON).Methods("GET")
+	router.HandleFunc("/", handleIndex).Methods("GET")
+	return router
 }
 
 func printCredits() {
